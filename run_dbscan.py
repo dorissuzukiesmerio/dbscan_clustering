361d48a5563028eb9794c137ebc51f43166f966a
dorissuzukiesmerio <dsuzuki@g.clemson.edu>
2022-01-04 12:08:36 -0500

    upload

--- run_dbscan.py
+++ run_dbscan.py
@@ -7,8 +7,8 @@ from sklearn.cluster import DBSCAN
 from sklearn import metrics
 
 
-# data = pandas.read_csv("dataset_moon.csv")
-data = pandas.read_csv("dataset_kmean.csv")
+data = pandas.read_csv("dataset_moon.csv")
+# data = pandas.read_csv("dataset_clustering.csv")
 
 print(data)
 
@@ -17,13 +17,17 @@ pyplot.scatter(data['x1'], data['x2'])
 pyplot.savefig("scatter.png")
 pyplot.close()
 
+
+#ALGORITHM IMPLEMENTATION:
 machine = DBSCAN(eps=0.49, min_samples=10)
 machine.fit(data)
 results = machine.labels_
 
+# VISUALIZATION OF RESULTS:
 print(results)
 pyplot.scatter(data['x1'], data['x2'], c=results)
 pyplot.savefig("scatter_with_color.png")
 pyplot.close()
 
+#MEASUREMENT OF PERFORMANCE:
 print(metrics.silhouette_score(data, results))
